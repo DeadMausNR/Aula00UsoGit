358f6d5adcee17540c318907f2b54508351f63e2
Nathan Lima <nathanlima123@gmail.com>
2020-03-10 11:48:33 -0300

    Adicionando construtor

--- Aluno.java
+++ Aluno.java
@@ -2,6 +2,11 @@ public class Aluno {
     private String nome;
     private int id;
 
+    public Aluno(String Nome, int Id){
+        nome = Nome;
+        id = Id;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
