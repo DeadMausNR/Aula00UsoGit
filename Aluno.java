ce22cefbcd491d06ac9e656a43849ee28fd01036
Nathan Lima <nathanlima123@gmail.com>
2020-03-10 11:57:36 -0300

    Ultimo pull

--- Aluno.java
+++ Aluno.java
@@ -1,21 +1,12 @@
 public class Aluno {
     private String nome;
     private int id;
-<<<<<<< HEAD
-
-    public Aluno(String Nome, int Id){
-        nome = Nome;
-        id = Id;
-    }
-
-=======
 	
     public Aluno(int id, String nome){
         this.nome = nome;
         this.id = id;
     }
 	
->>>>>>> 56d39169a9c192afd301aad27ad20b12dc28d645
     public void setNome(String nome) {
         this.nome = nome;
     }
